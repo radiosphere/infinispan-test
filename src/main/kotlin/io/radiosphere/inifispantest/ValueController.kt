7208dc970489b8573981d0fb844ed635a892715f
Johan Ablett-Karlsson <jak@radiosphere.com>
2021-11-12 11:39:18 +0100

    fix code to use infinispan

--- src/main/kotlin/io/radiosphere/inifispantest/ValueController.kt
+++ src/main/kotlin/io/radiosphere/inifispantest/ValueController.kt
@@ -1,21 +1,30 @@
 package io.radiosphere.inifispantest
 
+import org.infinispan.client.hotrod.RemoteCache
+import org.infinispan.client.hotrod.RemoteCacheManager
+import javax.inject.Inject
 import javax.ws.rs.*
 
 @Path("/values")
 class ValueController {
 
-    val store = mutableMapOf<String, String>()
+//    @Inject
+//    lateinit var cache: RemoteCache<String, String>
+
+    @Inject
+    lateinit var cacheManager: RemoteCacheManager
+
 
     @POST
     fun saveValue(@QueryParam("key") key: String, @QueryParam("value") value: String) {
-        store.put(key, value)
-
+        val cache  = cacheManager.getCache<String, String>("default")
+        cache.put(key, value)
     }
 
     @GET
     @Path("/{key}")
     fun saveValue(@PathParam(value = "key") key: String): String? {
-        return store[key]
+        val cache  = cacheManager.getCache<String, String>("default")
+        return cache[key]
     }
 }
